a090e73c2d9de90092fdd92f3ebe4eb61adfdb7b
Redsep3030 <3411938785@qq.com>
2024-01-05 20:24:33 +0800

    更新了窗口位置

--- __main__.py
+++ __main__.py
@@ -1,5 +1,15 @@
 from tkinter import *
+from os import *
 
 root = Tk()
+root.title('Auto_Name')
+WIDTH = str((root.winfo_screenwidth())//3)
+HEIGHT = str((root.winfo_screenheight())//5)
+root_size = '400x350+'+WIDTH+'+'+HEIGHT
+root.geometry(root_size)
+
+text = Label(root,text='Auto_Name昵称生成器')
+
+text.pack()
 
 root.mainloop()
