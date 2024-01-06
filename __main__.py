c76fcd0c1755484aa50d4ce381ab417a9722cf03
Redsep3030 <3411938785@qq.com>
2024-01-06 14:45:05 +0800

    优化某些与这些代码

--- __main__.py
+++ __main__.py
@@ -8,8 +8,10 @@ HEIGHT = str((root.winfo_screenheight())//5)
 root_size = '400x350+'+WIDTH+'+'+HEIGHT
 root.geometry(root_size)
 
-text = Label(root,text='Auto_Name昵称生成器',font=('TkCaptionFont',18))
+title = Label(root,text='Auto_Name昵称生成器',font=('TkCaptionFont',18))
+title.pack()
 
-text.pack()
+Start = Button()
+Start.pack()
 
 root.mainloop()
